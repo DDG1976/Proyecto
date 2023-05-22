615469667f747fd699c82009aface87109c6765e
DDG1976 <112675793+DDG1976@users.noreply.github.com>
2023-05-22 17:48:53 -0600

    Add files via upload

---
+++ Proyecto_Ahora_si_Chingonsote_XD_4/src/proyecto_ahora_si_chingonsote_xd/Proyecto_Ahora_si_Chingonsote_XD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_ahora_si_chingonsote_xd;
+
+/**
+ *
+ * @author diego
+ */
+public class Proyecto_Ahora_si_Chingonsote_XD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
